f57d41d4067807293dc5774d89c578330b99dae2
Adonics Nikolett <nikolett990106@gmail.com>
2023-11-23 23:51:31 +0100

    cnn all

--- model.py
+++ model.py
@@ -10,10 +10,10 @@ import numpy as np
 import math
 
 
-image_folder = 'logos/audi'
+image_folder = 'logos/all'
 output_folder = 'logos/output'
 
-bbox_file = 'logos/audi/audi.txt'
+bbox_file = 'logos/all/all.txt'
 
 box_colors = [(0, 0, 255)]
 box_thickness = 3
@@ -22,7 +22,6 @@ j = 0
 image = []
 image2 = []
 label_list = []
-label_l = []
 
 with open(bbox_file, 'r') as file:
     lines = file.readlines()
@@ -38,14 +37,14 @@ for line in lines:
             x0, y0, x1, y1, label, label_number = map(str.strip, parts[i:i+6])
             x0, y0, x1, y1, label_number = int(x0), int(y0), int(x1), int(y1), int(label_number)
 
-            kozeppont_x = (x1-x0) // 2
-            kozeppont_y = (y1-y0) // 2
-            kozep_x2 = x0 + kozeppont_x
-            kozep_y2 = y0 + kozeppont_y
-            rec_x0 = math.floor(kozep_x2 - 50)
-            rec_x1 = math.floor(kozep_x2 + 50)
-            rec_y0 = math.floor(kozep_y2 - 50)
-            rec_y1 = math.floor(kozep_y2 + 50)
+            center_pont_x = (x1-x0) // 2
+            center_pont_y = (y1-y0) // 2
+            center_x2 = x0 + center_pont_x
+            center_y2 = y0 + center_pont_y
+            rec_x0 = math.floor(center_x2 - 50)
+            rec_x1 = math.floor(center_x2 + 50)
+            rec_y0 = math.floor(center_y2 - 50)
+            rec_y1 = math.floor(center_y2 + 50)
 
             img = image[j]
 
@@ -66,10 +65,9 @@ image2 = image2 / 255.0
 class_names = ['apple', 'audi', 'barilla', 'bmw', 'mcdonalds', 'nike', 'other']
 label_np = np.array(label_list)
 
-# konvolúciós alap
-# Conv2D és MaxPooling2D rétegek
+# CNN konvolúciós neurális hálózat
 model = models.Sequential()
-# egy konvolúció egy 3x3-as szűrővel
+# egy konvolúciós réteg egy 3x3-as szűrővel
 model.add(layers.Conv2D(32, (3, 3), activation=relu, input_shape=(100, 100, 3)))
 # egy maximumkiválasztás egy 2x2-es sablont használva
 model.add(layers.MaxPooling2D((2, 2)))
