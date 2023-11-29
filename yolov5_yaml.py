54a25e90700e1d9e525e36a20cfbc16333f0d4d4
Adonics Nikolett <nikolett990106@gmail.com>
2023-11-29 23:37:18 +0100

    download yolov5 and create yaml

---
+++ yolov5_yaml.py
@@ -0,0 +1,20 @@
+import os.path
+import runpy
+
+from git import Repo
+
+
+def write_to_file():
+    f = open("yolov5/data/logo_detection.yaml", 'w')
+    f.write("path: ../logos \ntrain: train \ntest: test \n\nlogo_class: 6 \n\nlogos_names: \n 0: apple \n 1: audi \n" +
+            " 2: barilla \n 3: bmw \n 4: mcdonalds \n 5: nike")
+    f.close()
+
+
+if os.path.exists("yolov5"):
+    write_to_file()
+else:
+    Repo.clone_from("https://github.com/ultralytics/yolov5.git", "yolov5")
+    write_to_file()
+
+#Futtasd: pip install -r yolov5/requirements.txt
